a21f4738afb5f0c6518eab9c6d949e57128e6a50
Xu Qi <xuqi.jx@gmail.com>
2022-03-07 12:24:34 +0800

    更新数量不定的模版参数

--- tests/test_VariadicTemplates.cpp
+++ tests/test_VariadicTemplates.cpp
@@ -5,19 +5,30 @@
 #include <iostream>
 using namespace std;
 
+// 当参数个数为0时，调用的函数
 void print()
 {
-
+    cout <<"has no args " << endl;
 }
+
+// 参数类型可变，参数个数可变
 template <typename T,typename...Type>
 void print(const T& firstArg, const Type&... args)
 {
-    cout << firstArg << endl;
+    cout <<sizeof...(args) << ": "<< firstArg <<endl;
+    // 递归调用
     print(args...);
+}
+
+// 可以和上面的函数并存，并不会报错，只会选择特化的那个函数。
+template <typename... Type>
+void print(const Type&... args)
+{
+    cout << "call me ,args size = " << sizeof...(args) << endl;
 }
 
 int main()
 {
-    print(7.5,bitset<16>(377),42);
+    print(7.5,bitset<16>(377),42, "hello~");
     return 0;
 }
